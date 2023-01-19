7eba35212f29e6cee1347d67205a8f1c27dd57bc
Perkles <adryelleufrasio@yahoo.com.br>
2023-01-19 15:56:28 -0300

    meu-primeiro-projeto-java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("meus ovos");
+        System.out.println("meus ovos sao muito grandes e tu quer ver");
     }
 }
